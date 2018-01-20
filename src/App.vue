8b7164548547878125cc3462fadbd169b0c44c0f
Frederico Martins Porto <martinsporto@gmail.com>
2018-01-20 17:10:25 -0200

    Convert CurrencyList to use Bulma

--- src/App.vue
+++ src/App.vue
@@ -35,4 +35,10 @@ export default {
 </script>
 
 <style>
+.navbar {
+  height: 70px;
+  background: #fff;
+  border-radius: 2px;
+  box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
+}
 </style>
--- src/components/CurrencyList.vue
+++ src/components/CurrencyList.vue
@@ -1,41 +1,49 @@
 <template>
   <div class="block">
-    <nav class="level">
-      <div class="level-left"></div>
-      <div class="level-right">
-        <div class="field has-addons">
-          <p class="control">
-            <input type="text" class="input" placeholder="Enter keywords...">
-          </p>
-          <p class="control">
-            <a href="" class="button is-info">Search</a>
-          </p>
+    <div class="block">
+      <nav class="level">
+        <div class="level-left"></div>
+        <div class="level-right">
+          <div class="field has-addons">
+            <p class="control">
+              <input
+                type="text"
+                v-model="filterBy"
+                class="input search-bar"
+                placeholder="Search...">
+            </p>
+            <p class="control">
+              <a class="button is-info" @click="filter()">Search</a>
+            </p>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </div>
     <div class="block">
       <table class="table is-striped is-narrow is-hoverable is-fullwidth">
           <thead>
             <th v-for="h in headers" :key="h.value">
-              <td :id="h.value" :class="'has-text-' + h.align">{{ h.text }}</td>
+              <td :id="h.value" :class="'table-header table-text has-text-' + h.align">
+                <a @click="orderBy(h.value)">{{ h.text }}</a>
+              </td>
             </th>
           </thead>
           <tbody>
-            <tr v-for="c in currencies" :key="c.name">
-              <td class="has-text-centered">{{ c.rank }}</td>
-              <td class="has-text-left">{{ c.name }}</td>
-              <td class="has-text-centered">{{ c.symbol }}</td>
-              <td class="has-text-right">{{ c.marketCap | round }}</td>
-              <td class="has-text-right">{{ c.price | round }}</td>
-              <td class="has-text-right">{{ c.volume24h | round }}</td>
-              <td class="has-text-right">{{ c.percentChange1h | round }}</td>
-              <td class="has-text-right">{{ c.percentChange24h | round }}</td>
-              <td class="has-text-right">{{ c.percentChange7d | round }}</td>
-              <td class="has-text-centered">{{ c.liquidity }}</td>
-              <td class="has-text-centered">{{ c.inflationary | humanizeBoolean }}</td>
-              <td class="has-text-right">{{ c.maxPrice | round }}</td>
-              <td class="has-text-right">{{ c.fairPrice | round }}</td>
-              <td class="has-text-right">{{ c.growthPotential | round }} %</td>
+            <tr v-for="c in orderedCurrencies" :key="c.name">
+              <td class="table-text has-text-centered">{{ c.rank }}</td>
+              <td class="table-text has-text-left">{{ c.name }}</td>
+              <td class="table-text has-text-centered">{{ c.symbol }}</td>
+              <td class="table-text has-text-right">{{ c.marketCap | round }}</td>
+              <td class="table-text has-text-right">{{ c.price | round }}</td>
+              <td class="table-text has-text-right">{{ c.volume24h | round }}</td>
+              <td class="table-text has-text-right">{{ c.percentChange1h | round }}</td>
+              <td class="table-text has-text-right">{{ c.percentChange24h | round }}</td>
+              <td class="table-text has-text-right">{{ c.percentChange7d | round }}</td>
+              <td class="table-text has-text-centered">{{ c.liquidity }}</td>
+              <td class="table-text has-text-centered">{{ c.inflationary | humanizeBoolean }}</td>
+              <td class="table-text has-text-right">{{ c.maxPrice | round }}</td>
+              <td class="table-text has-text-right">{{ c.fairPrice | round }}</td>
+              <td class="table-text has-text-right">{{ c.growthPotential | round }} %</td>
             </tr>
           </tbody>
         </table>
@@ -44,6 +52,8 @@
 </template>
 
 <script>
+import _ from 'lodash';
+
 export default {
   data() {
     return {
@@ -60,11 +70,44 @@ export default {
         { text: '% 7d', value: 'percentChange7d', align: 'right' },
         { text: 'Liquidity', value: 'liquidity', align: 'centered' },
         { text: 'Inflationary', value: 'inflationary', align: 'centered' },
-        { text: 'Max Price', value: 'maxPrice', align: 'right' },
-        { text: 'Fair Price', value: 'fairPrice', align: 'right' },
-        { text: 'Growth Potential', value: 'growthPotential', align: 'right' },
+        { text: 'Max Price', value: 'maxPrice', align: 'centered' },
+        { text: 'Fair Price', value: 'fairPrice', align: 'centered' },
+        { text: 'Growth Potential', value: 'growthPotential', align: 'centered' },
       ],
+      orderedCurrencies: [],
+      orderedBy: {},
+      filterBy: '',
     };
+  },
+  watch: {
+    filterBy() {
+      this.filter();
+    },
+  },
+  methods: {
+    orderBy(field) {
+      const textFields = ['name', 'symbol', 'inflationary'];
+      if (field === this.orderedBy.field) {
+        this.orderedBy.order = this.orderedBy.order === 'asc' ? 'desc' : 'asc';
+      } else {
+        this.orderedBy = { field, order: 'asc' };
+      }
+      this.orderedCurrencies = _.orderBy(
+        this.currencies,
+        (obj) => {
+          const isText = textFields.includes(obj[this.orderedBy.field]);
+          return isText ? obj[this.orderedBy.field] : parseInt(obj[this.orderedBy.field], 10);
+        },
+        this.orderedBy.order,
+      );
+    },
+    filter() {
+      this.orderedCurrencies = this.currencies.filter((currency) => {
+        if (this.filterBy === '') return true;
+        const keys = Object.keys(currency);
+        return keys.some(key => (currency[key] || '').toString().toLowerCase().includes(this.filterBy.toLowerCase()));
+      });
+    },
   },
   computed: {
     currencies() {
@@ -72,7 +115,9 @@ export default {
     },
   },
   mounted() {
-    this.$store.dispatch('fetchCurrencies');
+    this.$store.dispatch('fetchCurrencies').then(() => {
+      this.orderBy('rank');
+    });
   },
 };
 </script>
@@ -82,5 +127,28 @@ export default {
   .has-addons {
     margin-top: 40px;
     margin-bottom: 25px;
+  }
+  .search-bar {
+    width: 300px;
+  }
+  .table {
+    min-width: 600px;
+    overflow-x: auto;
+    background: #fff;
+    border-radius: 2px;
+    box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
+  }
+  .table-div {
+    white-space:nowrap;
+    overflow: hidden;
+  }
+  .table-text {
+    font-size: 13px;
+    height: 40px;
+    line-height: 20px;
+    vertical-align: middle;
+  }
+  .table-header {
+    color: rgba(0, 0, 0, 0.565);
   }
 </style>
