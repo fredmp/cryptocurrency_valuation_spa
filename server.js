caebb413fe913f6e20ad270f3ae27a6a7e8aff63
Frederico Martins Porto <martinsporto@gmail.com>
2018-02-18 12:01:43 -0300

    Improve deploy process

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
+/* eslint-disable */
 var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
 
 app = express();
 
-app.use(serveStatic(__dirname));
+app.use(serveStatic(path.join(__dirname, 'dist')));
 
 app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 var port = process.env.PORT || 5000;
